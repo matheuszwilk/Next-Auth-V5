5cb63d9121abdde147d1186de6e7e84fbdf14837
Matheus Pereira <matheuszwilk@gmail.com>
2024-05-07 19:04:58 -0400

    feat: ensure Skeleton

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,10 +2,11 @@ import { GlobeLogin } from '@/components/auth/globe-form'
 import { LoginForm } from '@/components/auth/login-form'
 import React from 'react'
 import { Suspense } from 'react'
+import Skeleton from 'react-loading-skeleton'
 
 const LoginPage = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Skeleton />}>
       <div className="lg:grid lg:h-full lg:w-full lg:grid-cols-2 xl:h-full xl:w-full xl:grid-cols-2">
         <div className="w-full flex-col gap-y-4 items-center justify-center bg-muted lg:block hidden">
           <GlobeLogin />
