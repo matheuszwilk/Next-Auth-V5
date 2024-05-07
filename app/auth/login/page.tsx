851b0046abf28d6600b4fdcabc6b0a04fe1cc4b1
Matheus Pereira <matheuszwilk@gmail.com>
2024-05-06 21:17:52 -0400

    fix: globe

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,18 +1,20 @@
 import { GlobeLogin } from '@/components/auth/globe-form'
 import { LoginForm } from '@/components/auth/login-form'
-import Image from 'next/image'
 import React from 'react'
+import { Suspense } from 'react'
 
 const LoginPage = () => {
   return (
-    <div className="lg:grid lg:h-full lg:w-full lg:grid-cols-2 xl:h-full xl:w-full xl:grid-cols-2">
-      <div className="w-full flex-col gap-y-4 items-center justify-center bg-muted lg:block hidden">
-        <GlobeLogin />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="lg:grid lg:h-full lg:w-full lg:grid-cols-2 xl:h-full xl:w-full xl:grid-cols-2">
+        <div className="w-full flex-col gap-y-4 items-center justify-center bg-muted lg:block hidden">
+          <GlobeLogin />
+        </div>
+        <div className="w-full flex flex-col items-center justify-center">
+          <LoginForm />
+        </div>
       </div>
-      <div className="w-full flex flex-col items-center justify-center">
-        <LoginForm />
-      </div>
-    </div>
+    </Suspense>
   )
 }
 
